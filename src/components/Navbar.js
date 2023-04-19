7ff5850bbcf3ce25f79da82e3591d45a2e770fa3
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-04-19 13:52:35 +0700

    add navbar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import React from 'react'
 import Logo from './Logo';
 import { useRouter } from 'next/router';
+import { EmailIcon, GithubIcon, LinkedInIcon } from './Icons';
+import { motion } from 'framer-motion';
 
 const CustomLink = ({ href, title, className="" }) => {
     const router = useRouter();
@@ -13,7 +15,6 @@ const CustomLink = ({ href, title, className="" }) => {
             className={`
                 h-[1px] 
                 inline-block 
-                w-0 
                 bg-dark 
                 absolute 
                 left-0 
@@ -22,6 +23,7 @@ const CustomLink = ({ href, title, className="" }) => {
                 transition-[width]
                 ease
                 duration-300
+                ${router.asPath === href ? 'w-full' : 'w-0'}
                 `}
             >
                 &nbsp;
@@ -39,13 +41,47 @@ const Navbar = () => {
                 <CustomLink href='/projects' title='Projects' className='ml-4'/>
             </nav>
             
-            <nav>
-                <Link href='/' target={'_blank'}>T</Link>
-                <Link href='/' target={'_blank'}>T</Link>
-                <Link href='/' target={'_blank'}>T</Link>
-                <Link href='/' target={'_blank'}>T</Link>
-                <Link href='/' target={'_blank'}>T</Link>
-                <Link href='/' target={'_blank'}>T</Link>
+            <nav className='flex items-center justify-center flex-wrap'>
+                <motion.a 
+                    className='w-6 mr-2'
+                    href='mailto:mukhtar.r.f@gmail.com' 
+                    target={'_blank'}
+                    whileHover={{
+                        y: -2,
+                    }}
+                    whileTap={{
+                        scale: 0.9
+                    }}
+                >
+                <EmailIcon />
+                </motion.a>
+                <motion.a 
+                    className='w-6 mx-2'
+                    href='https://github.com/rafizuaf' 
+                    target={'_blank'}
+                    whileHover={{
+                        y: -2,
+                    }}
+                    whileTap={{
+                        scale: 0.9
+                    }}
+                    >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a 
+                    className='w-6 ml-2'
+                    href='https://www.linkedin.com/in/rafizuaf/' 
+                    target={'_blank'}
+                    whileHover={{
+                        y: -2,
+                    }}
+                    whileTap={{
+                        scale: 0.9
+                    }}
+                >
+                    <LinkedInIcon />
+                </motion.a>
+                
             </nav>
 
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
