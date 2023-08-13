d40fe09dae0f993a7120ae9a879a7c49e8c0f4c4
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-08-14 01:12:20 +0700

    fix miinor bugs on skill

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ import Experience from "@/components/Experience";
 import Education from "@/components/Education";
 import TransitionEffect from "@/components/TransitionEffect";
 
-import ProfilePicture from "../../public/images/photo.png";
+import ProfilePicture from "../../public/images/FF_2017.jpg";
 
 const AnimatedNumbers = ({ value }) => {
     const ref = useRef(null);
